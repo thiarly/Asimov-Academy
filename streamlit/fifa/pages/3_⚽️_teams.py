97ab80f08dde41ddb9e79d1ae1e6f6330e206b14
Thiarly Cavalcante <thiarly.cavalcante@live.com>
2023-11-24 14:24:55 -0300

    finish streamlit fifa

--- streamlit/fifa/pages/3_⚽️_teams.py
+++ streamlit/fifa/pages/3_⚽️_teams.py
@@ -12,7 +12,23 @@ df_data = st.session_state["data"]
 clubes = df_data["Club"].value_counts().index
 club = st.selectbox("Selecione o clube", clubes)
 
-df_filtered_club = df_data[df_data["Club"] == club]
+df_filtered = df_data[(df_data["Club"] == club)].set_index("Name")
 
-st.image(df_filtered_club["Photo"].iloc[0])
-st.title(df_filtered_club["Club"].iloc[0])
+st.image(df_filtered.iloc[0]["Club Logo"])
+st.markdown(f"## {club}")
+
+columns = ["Age", "Photo", "Flag", "Overall",  "Value(£)", "Wage(£)", "Joined",
+            'Height(cm.)', 'Weight(lbs.)', 'Contract Valid Until', 'Release Clause(£)']    
+
+
+st.dataframe(df_filtered[columns],
+             column_config={
+                 "Overall": st.column_config.ProgressColumn(
+                        "Overall", format="%d", min_value=0, max_value=100
+                     ),
+                  "Wage(£)": st.column_config.ProgressColumn(
+                        "Weekly Wage", format="£%d", 
+                        min_value=0, max_value=df_filtered["Wage(£)"].max()),
+                  "Photo": st.column_config.ImageColumn("Photo"),
+                  "Flag": st.column_config.ImageColumn("Country"),
+             })
