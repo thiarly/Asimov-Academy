2202faf1ad48f88c2278fff01a7bd757611d6f61
Thiarly Cavalcante <thiarly.cavalcante@live.com>
2023-11-28 09:47:46 -0300

    Estrutura básica app finance

---
+++ finance/myindex.py
@@ -0,0 +1,40 @@
+from dash import html, dcc
+import dash
+from dash.dependencies import Input, Output
+import dash_bootstrap_components as dbc
+import pandas as pd
+import plotly.express as px
+
+from app import *
+from components import sidebar, extratos, dashboards
+
+
+
+
+# =========  Layout  =========== #
+content = html.Div(id="page-content")
+
+
+app.layout = dbc.Container(children=[
+    dbc.Row([
+        dbc.Col([
+            dcc.Location(id="url"),
+            sidebar.layout
+        ], md=2, style={"background-color": "red", 'height': "1080px", 'padding': '0px'}),
+        dbc.Col([
+            content
+        ], md=10, style={"background-color": "blue", 'height': "1080px", 'padding': '0px'})
+    ])
+    
+], fluid=True,)
+
+@app.callback(Output("page-content", "children"), [Input("url", "pathname")])
+def render_page_content(pathname):
+    if pathname == "/" or pathname == "/dashboard":
+        return dashboards.layout
+    if pathname == "/extratos":
+        return extratos.layout
+
+
+if __name__ == '__main__':
+    app.run_server(port=8051, debug=True)
