ef45e1c9b1b2208d88bc72643248e002acd0ddbd
Thiarly Cavalcante <thiarly.cavalcante@live.com>
2023-11-28 10:15:21 -0300

    construção sidebar

--- finance/components/sidebar.py
+++ finance/components/sidebar.py
@@ -14,13 +14,37 @@ import pandas as pd
 
 
 
-
 # ========= Layout ========= #
 layout = dbc.Col([
-                
-            ])
+    html.H1("MyBudget", className="text-primary"),
+    html.P("By Thiarly Cavalcante.", className="text-input"),
+    html.Hr(),
+            
+    # Seção PERFIL
+    dbc.Button(id="botao_avatar",
+               children=[html.Img(src="/assets/img_hom.png", id="avatar_change", alt='Avatar', className="perfil_avatar")
+    ], style={"background-color": "transparent", 'border-color': "transparent"}),  # Mantenha a vírgula aqui
+    
+    # Seção NOVO
+    dbc.Row([
+        dbc.Col([
+            dbc.Button(color='success', id='open-novo-receita',
+                       children=['+ Receita']),
+        ], width=6),
+        dbc.Col([
+            dbc.Button(color='danger', id='open-novo-despesa',
+                       children=['- Despesa']),
+        ], width=6),    
+    ]),  # Adicione uma vírgula aqui para separar esta seção da próxima
 
-
+    # Seção NAVBAR
+    html.Hr(),
+    dbc.Nav(
+        [
+            dbc.NavLink("Dashboard", href="/dashboard", active="exact"),
+            dbc.NavLink("Extratos", href="/extratos", active="exact"),
+        ], vertical=True, pills=True, id="nav-buttons", style={"margin-bottom": "50px"}),
+], id="sidebar_completa")
 
 
 
